94f27a8f3688e9fffa7db9bdf622417f441eb6ec
Juan Pablo <jpvjpruebas1@gmail.com>
2025-05-11 22:55:30 -0500

    Update from get to post endpoint

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -21,7 +21,7 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/activate", activateUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/logout", isAuthenticated, logoutUser);
+userRouter.post("/logout", isAuthenticated, logoutUser);
 // userRouter.get("/refresh", updateAccessToken);
 userRouter.get(
   "/me",
